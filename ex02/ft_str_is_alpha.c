bc863051d3536807f021951ce463828223378638
antoine-2beco <antoinedb@RB-MA.local>
2023-09-10 19:46:13 +0200

    ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -9,42 +9,24 @@
 /*   Updated: 2023/09/08 20:28:39 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
 int	ft_str_is_alpha(char *str)
 {
-	int counter;
-	char temp;
+	int	i;
+	char	temp;
 
-	counter = 0;
-
-	while (&str[counter] != '\0')
+	i = 0;
+	while (*str)
 	{
-		temp = str[counter];
-		write(1, &str[counter], 1);
-		if ((97 <= temp) || (temp <= 122) || (65 <= temp) || (temp <= 90))
+		temp = str[i];
+		if ((str[i] < 'a') || (str[i] > 'z'))
 		{
-			counter++;
+			if ((str[i] < 'A') || (str[i] > 'Z'))
+			{
+				return (0);
+			}
 		}
-		else
-		{
-			write(1, "0", 1);
-			return (0);
-		}
+	str++;
 	}
-	write(1, "1", 1);
 	return (1);
-}
-
-int	main(void)
-{	
-	char str[]  = {"!58fds545"};
-	ft_str_is_alpha(str);
-
-	char str2[3];
-	//ft_str_is_alpha(str2);
-
-	char str3[] = {"!58545"};
-	ft_str_is_alpha(str3);
-	return (0);
 }
