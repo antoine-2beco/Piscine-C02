f5df0462af0d4d3acde8ce9fb8ba737a4c594e57
antoine-2beco <antoinedb@RB-MA.local>
2023-09-10 18:22:39 +0200

    ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -9,15 +9,42 @@
 /*   Updated: 2023/09/08 20:28:39 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-char	*ft_strcpy(char *dest, char *src)
+int	ft_str_is_alpha(char *str)
 {
+	int counter;
+	char temp;
 
+	counter = 0;
+
+	while (&str[counter] != '\0')
+	{
+		temp = str[counter];
+		write(1, &str[counter], 1);
+		if ((97 <= temp) || (temp <= 122) || (65 <= temp) || (temp <= 90))
+		{
+			counter++;
+		}
+		else
+		{
+			write(1, "0", 1);
+			return (0);
+		}
+	}
+	write(1, "1", 1);
+	return (1);
 }
 
-int main(void)
-{
-	ft_strcpy();
+int	main(void)
+{	
+	char str[]  = {"!58fds545"};
+	ft_str_is_alpha(str);
+
+	char str2[3];
+	//ft_str_is_alpha(str2);
+
+	char str3[] = {"!58545"};
+	ft_str_is_alpha(str3);
 	return (0);
 }
-
